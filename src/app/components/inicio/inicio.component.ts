63bbaf1b65ee4fb4dad13920b472d0023d59aaf8
EricAlonsoSevillanoCES <114073141+EricAlonsoSevillanoCES@users.noreply.github.com>
2024-06-09 20:46:27 +0200

    commit 2

---
+++ src/app/components/inicio/inicio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css',
+})
+export class InicioComponent {
+  logoUrl: string = 'assets/Logo.PNG';
+  flechasUrl: string = 'assets/flechas.png';
+}
